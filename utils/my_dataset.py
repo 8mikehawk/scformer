9752b9dfc3ee58fa1fab4aef209aa407dd6cb3a5
Qiming-Huang <727534525@qq.com>
2021-12-25 14:35:14 +0800

    add dataset

--- utils/my_dataset.py
+++ utils/my_dataset.py
@@ -9,7 +9,7 @@ import torchvision.transforms as transforms
 from cv2 import cv2
 import torch
 
-
+# dataset for isic2018
 class ISIC2018(Dataset):
     def __init__(self, train_img_root, val_img_root, train_label_root, val_label_root, crop_size, mode='train'):
         self.train_img_files = self.read_file(train_img_root)
@@ -68,11 +68,61 @@ class ISIC2018(Dataset):
         file_path_list.sort()
         return file_path_list        
 
-# train_img_root = "/mnt/DATA-1/DATA-2/Feilong/scformer/data/ISIC2018/train/"
-# val_img_root = "/mnt/DATA-1/DATA-2/Feilong/scformer/data/ISIC2018/val/"
-# train_label_root = "/mnt/DATA-1/DATA-2/Feilong/scformer/data/ISIC2018/train_labels/"
-# val_label_root = "/mnt/DATA-1/DATA-2/Feilong/scformer/data/ISIC2018/val_labels/"
-# resize = (512, 512)
+# dataset fro Kvasir
+class Kvasir(Dataset):
+    def __init__(self, train_img_root, val_img_root, train_label_root, val_label_root, crop_size, mode='train'):
+        self.train_img_files = self.read_file(train_img_root)
+        self.val_img_files = self.read_file(val_img_root)
+        self.train_label_files = self.read_file(train_label_root)
+        self.val_label_files = self.read_file(val_label_root)
+        self.mode = mode
+        self.crop_size = crop_size
 
-# train_ds = ISIC2018(train_img_root, val_img_root, train_label_root, val_label_root, resize, mode='train')
-# val_ds = ISIC2018(train_img_root, val_img_root, train_label_root, val_label_root, resize, mode='val')
+    def __getitem__(self, index):
+        if self.mode == 'train':
+            img = Image.open(self.train_img_files[index])
+            label = Image.open(self.train_label_files[index])
+
+            img = img.resize((self.crop_size[0], self.crop_size[1]))
+            label = label.resize((self.crop_size[0], self.crop_size[1]))
+
+            img = np.array(img) / 255
+            label = np.array(label)
+
+            img = torch.as_tensor(img)
+            label = torch.as_tensor(label)
+
+            img = img.permute(2, 0, 1)
+
+            return img.float(), label.long()
+
+        if self.mode == 'val':
+            img = Image.open(self.val_img_files[index])
+            label = Image.open(self.val_label_files[index])
+
+            img = img.resize((self.crop_size[0], self.crop_size[1]))
+            label = label.resize((self.crop_size[0], self.crop_size[1]))
+
+            img = np.array(img) / 255
+            label = np.array(label)
+
+            img = torch.as_tensor(img)
+            label = torch.as_tensor(label)
+
+            img = img.permute(2, 0, 1)
+
+            return img.float(), label.long()       
+
+    def __len__(self):
+        if self.mode == 'train':
+            total_img = len(self.train_img_files)
+            return total_img
+        if self.mode == 'val':
+            total_img = len(self.val_img_files)
+            return total_img
+
+    def read_file(self, path):
+        files_list = os.listdir(path)
+        file_path_list = [os.path.join(path, img) for img in files_list]
+        file_path_list.sort()
+        return file_path_list   
