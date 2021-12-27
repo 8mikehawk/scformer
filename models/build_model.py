42e144f001677bfa1eeb93bab617180b893b1ae9
root <root@localhost.localdomain>
2021-12-27 13:22:16 +0800

    add build model

---
+++ models/build_model.py
@@ -0,0 +1,22 @@
+from .sct import sct_b1
+from .sct_dw import sct_b1 as sct_dw_b1
+from .sct_pixel import sct_b1 as sct_pixel_b1
+from .sct_dw_pixel import sct_b1 as sct_dw_pixel_b1
+
+
+def build(model_name, class_num=2):
+    if model_name == "sct_b1":
+        model = sct_b1(class_num=class_num)
+        return model
+
+    if model_name == "sct_dw_b1":
+        model = sct_dw_b1(class_num=class_num)
+        return model
+
+    if model_name == "sct_pixel_b1":
+        model = sct_pixel_b1(class_num=class_num)
+        return model
+
+    if model_name == "sct_dw_pixel_b1":
+        model = sct_dw_pixel_b1(class_num=class_num)
+        return model    
